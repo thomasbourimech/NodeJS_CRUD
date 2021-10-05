7949e3f3b97df77a8e7d88f33704bde5b36fc32a
bourim <bourim@wni.com>
2021-10-05 21:27:10 +0200

    Ajout Sequelize + Proto Archi

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const sqlite3 = require("sqlite3").verbose()
+const db_name = path.join(__dirname, "data", "users.db");
+const connection = require("./dao/connection")
+const connectionInstance = new connection(db_name)
+const db = connectionInstance.get_connection()
+
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
     {
@@ -9,6 +21,23 @@ app.listen(3000, () => {
 
 app.get("/", (req, res) => {
     {
-        res.send ("Hello world...");
+
+        res.render("index");
     }
 });
+
+app.get("/about", (req, res) => {
+    {
+        res.render("about");
+    }
+});
+
+app.get("/data", (req, res) => {
+    const test = {
+        title: "Liste du catalogue",
+        items: ["Ville", "Département", "Région"]
+    };
+
+    res.render("data", {model: test});
+});
+
---
+++ dao/connection.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const sqlite3 = require("sqlite3").verbose()
+
+class Connection {
+
+    constructor(db_path){
+        this.db_name = path.join(db_path);
+    }
+
+    get_connection(){
+        this.db = new sqlite3.Database(this.db_name, err => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(`Connexion to db successful ${this.db_name}`);
+        });
+        return this.db
+    }
+
+}
+
+module.exports = Connection
+
+// const sql_create = `CREATE TABLE IF NOT EXISTS Books (
+//   Book_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+//   Title VARCHAR(100) NOT NULL,
+//   Author VARCHAR(100) NOT NULL,
+//   Comments TEXT
+// );`;
+//
+// db.run(sql_create, err => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log("Successful creation of the 'Books' table");
+// });
